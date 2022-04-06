4ba5569874530e8aaf71e87c88b78ae62112f464
jlangel19 <99676351+jlangel19@users.noreply.github.com>
2022-04-05 22:59:28 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,1 +1,24 @@
+var playing = false;
+//var currentPlayer; 
+const timerPanel = document.getElementsByClassName("timer");
 
+//Create a class for the timer
+class Timer {
+  constructor(player, minutes) {
+    this.player = player;
+    this.minutes = minutes;
+  }
+  
+  getMinutes(timeId) {
+    return document.getElementById(timeId).textContent;
+  }
+}
+
+//Add a leading zero to numbers less than 10
+const padZero = function(number) {
+  if (number < 10) {
+    return "0" + number;
+  } else {
+    return number;
+  }
+}

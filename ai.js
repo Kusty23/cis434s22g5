c039f4e66e3cc19a3914f51a22eb9ed0722c2843
John Langel <99676351+jlangel19@users.noreply.github.com>
2022-04-18 15:16:51 -0400

    Update ai.js

--- ai.js
+++ ai.js
@@ -23,26 +23,105 @@ function cloneGameBoard(board) {
 
 }
 
-function calculateBestMove() {
+function calculateBestMove(board, depth, isMaximizingPlayer) {
 	var possibleMoves = getAllBlackPieceMoves();
 	var bestMove;
 	var bestValue = -Infinity;
 
 	for (var i = 0; i < possibleMoves.length; i++) {
-		var pboard1 = cloneGameBoard(gameboard);
+		var pboard = cloneGameBoard(board);
 
 		var move = possibleMoves[i];
+		pboard[move[0]].updatePosition(move[1], true);
 
+		console.log("test1");
+		var value = minimax(pboard, depth - 1, !isMaximizingPlayer);
 
+		if (value >= bestValue) {
+			bestValue = value;
+			bestMove = move;
+		}
+
+		/*
 		// Take the negative as AI is the black (minimizing) player
-		var boardValue = evaluateBoard(pboard1);
+		//var boardValue = evaluateBoard(pboard1);
 		if (boardValue >= bestValue) {
 			bestValue = boardValue;
 			bestMove = move;
-		}
+		} */
 	}
 
 	return bestMove;
+}
+
+function minimax(board, depth, isMaximizingPlayer) {
+	if (depth == 0) {
+		
+		console.log("test2");
+		return evaluateBoard(board);
+	}
+	
+	console.log("test3");
+
+	var pboard = cloneGameBoard(board);
+	if (isMaximizingPlayer) {																			// Where issues are occurring
+		console.log("test5");
+
+		var moves = [];
+		
+		for (var i = 0; i < 64; i++) {
+			if (pboard[i] instanceof Phantom_Piece && pboard[i].color == "white") {
+				for (var j = 0; j < pboard[i].getValidMoves().length; j++) {
+					moves.push([pboard[i].position, pboard[i].getValidMoves()[j]]);
+				}
+			}
+		}
+
+		var bestValue = -Infinity;
+		for (var i = 0; i < moves.length; i++) {
+			var move = moves[i];
+			pboard[move[0]].updatePosition(move[1], false);
+			
+			console.log(pboard);
+			console.log(depth);
+			console.log(isMaximizingPlayer);
+
+			let value = minimax(pboard, depth - 1, !isMaximizingPlayer);
+			if (value > bestValue) {
+				bestValue = value;
+			}
+		}
+
+		return bestValue;
+	} else {																							// Where issues are occurring.
+		var moves = [];
+		
+		console.log("test4");
+		for (let i = 0; i < 64; i++) {
+			if (pboard[i] instanceof Phantom_Piece && pboard[i].color == "black") {
+				for (var j = 0; j < pboard[i].getValidMoves().length; j++) {
+					moves.push([pboard[i].position, pboard[i].getValidMoves()[j]]);
+				}
+			}
+		}
+
+		var bestValue = Infinity;
+		for (var i = 0; i < moves.length; i++) {
+			var move = moves[i];
+			pboard[move[0]].updatePosition(move[1], true);
+
+			console.log(pboard);
+			console.log(depth);
+			console.log(isMaximizingPlayer);
+
+			let value = minimax(pboard, depth - 1, !isMaximizingPlayer);
+			if (value < bestValue) {
+				bestValue = value;
+			}
+		}
+
+		return bestValue;
+	}
 }
 
 function evaluateBoard(board) {
@@ -54,44 +133,27 @@ function evaluateBoard(board) {
 	return totalEval;
 }
 
-function getPieceValue(piece) {
-	if (!(piece instanceof Piece)) {
+function getPieceValue(phantom_piece) {
+	if (!(phantom_piece instanceof Phantom_Piece)) {
 		return 0;
 	}
 
-	function pieceValue(piece) {
-		if (piece instanceof Piece && piece.symbol == PAWN) {
+	function pieceValue(phantom_piece) {
+		if (phantom_piece instanceof Phantom_Pawn) {
 			return 10;
-		} else if (piece instanceof Piece && piece.symbol == KNIGHT) {
+		} else if (phantom_piece instanceof Phantom_Knight) {
 			return 30;
-		} else if (piece instanceof Piece && piece.symbol == BISHOP) {
+		} else if (phantom_piece instanceof Phantom_Bishop) {
 			return 30;
-		} else if (piece instanceof Piece && piece.symbol == ROOK) {
+		} else if (phantom_piece instanceof Phantom_Rook) {
 			return 50;
-		} else if (piece instanceof Piece && piece.symbol == QUEEN) {
+		} else if (phantom_piece instanceof Phantom_Queen) {
 			return 90;
-		} else if (piece instanceof Piece && piece.symbol == KING) {
+		} else if (phantom_piece instanceof Phantom_King) {
 			return 900;
 		}
 	};
 
-	var pieceValue = pieceValue(piece);
-	return piece instanceof Piece && piece.color == 'white' ? pieceValue : -pieceValue;
+	var pieceValue = pieceValue(phantom_piece);
+	return phantom_piece instanceof Phantom_Piece && phantom_piece.color == 'white' ? pieceValue : -pieceValue;
 }
-
-
-/*
-if (mode == 'pvc' || mode == 'cvc') {
-	let moved = false;
-	while (!moved) {
-		let piece = blackPieces[parseInt(Math.random() * blackPieces.length)];
-		let moves = piece.getValidMoves();
-		if (piece.alive && moves.length > 0) {
-			let move = calculateBestMove(moves); 
-			console.log(piece);
-			console.log('was moved by black to ' + move);
-			piece.updatePosition(move, true);
-			moved = true;
-		}
-	}
-} */
